6e51a90e250a6cf2e30c5a8b0a041d1cb0ee4461
altuntasomer <54469544+altuntasomer@users.noreply.github.com>
2020-08-20 17:49:50 +0300

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+int array[2];
+int size = 2;
+int top = 0;
+void push(int);
+int pop();
+void print();
+int main()
+{
+	push(10);
+	push(15);
+	push(20);
+	print();
+	printf("%d ", pop());
+	printf("%d ", pop());
+	printf("%d ", pop());
+	printf("%d ", pop());
+	return 0;
+}
+void push(int data)
+{
+	
+	array[top++] = data;
+}
+int pop()
+{
+	return array[--top];
+}
+void print()
+{
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%d ", array[i]);
+	}
+}
